c768e611a3f1f7f4f5a1fd81b4ec7b8df47eb486
paintt23 <titinan.loawkul@gmail.com>
2023-08-08 17:02:50 +0700

    success

---
+++ 01-If-Else/05-oddEven/main.js
@@ -0,0 +1,9 @@
+ let b = (prompt("กรุณาใส่ตัวเลข: "));
+
+if (b % 2 == 0){
+    alert("Even number");
+}else{
+    alert("Odd number");
+}
+
+console.log(b)
---
+++ 01-If-Else/11-ternary/main.js
@@ -0,0 +1,4 @@
+
+let login = prompt("Enter username");
+message =login == 'Employee'?'Hello' :login ==='Director'? 'Greeting':login ==''? 'No login' :"";
+alert(message) ;
